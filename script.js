0ebe8bd9ad7e9967244b0e6a5c7c6cca75d24eab
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-04-22 18:30:46 +0530

    created js file

---
+++ script.js
@@ -0,0 +1,8 @@
+const form1 = document.querySelector('.note1');
+const form2 = document.querySelector('.note2');
+
+form1.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    
+})
